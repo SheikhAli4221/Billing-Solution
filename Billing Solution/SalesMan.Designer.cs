70b30aed9a80bb95918a701112f48e775478030d
Zaheem Sajid <ART@DESKTOP-4VFJU86>
2025-03-25 20:00:08 +0500

    increment by 1

--- Billing Solution/SalesMan.Designer.cs
+++ Billing Solution/SalesMan.Designer.cs
@@ -37,7 +37,7 @@
             button1.Name = "button1";
             button1.Size = new Size(94, 29);
             button1.TabIndex = 0;
-            button1.Text = "ben 10";
+            button1.Text = "ben 11";
             button1.UseVisualStyleBackColor = true;
             // 
             // SalesMan
